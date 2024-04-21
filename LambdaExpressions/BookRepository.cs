f9902ce4b25bf5ae2d9adb5b00110bc81f3baaec
JavadFazelpour <Javad_Fazelpour@outlook.com>
2024-04-21 17:17:02 +0330

    Using a function that returns bool as a predicate for LINQ FindAll().

---
+++ LambdaExpressions/BookRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions;
+internal class BookRepository
+{
+    public BookRepository()
+    {
+        
+    }
+    public List<Book> GetBooks()
+    {
+        return new List<Book> {
+        new(){Title="LINQ in Action",Price=23.65f},
+        new(){Title="EF Core in Action",Price=36.95f},
+        new(){Title="ASP .NET Core in Action",Price=45.99f},
+        new(){Title="Database System concepts 7",Price=59.37f},
+        new(){Title=".NET MAUI in Action",Price=65.5f},
+        new(){Title="Head First C# 5th edition",Price=50.65f},
+        };
+    }
+}
--- LambdaExpressions/Program.cs
+++ LambdaExpressions/Program.cs
@@ -2,19 +2,30 @@
 
 internal class Program
 {
+    static bool IsCheaperThan50Dollars(Book book)
+    {
+        return book.Price < 50.0;
+    }
     static void Main(string[] args)
     {
         // args => expression
 
         // () => expression
         // x => expression
-        //(x,y,z) => expression
+        // (x,y,z) => expression
 
-        Func<int, int> square = number => number * number;
-        Console.WriteLine($"Using lambda expression 'square': {square(6)}");
+        var books = new BookRepository().GetBooks();
+        var cheapBooks = books.FindAll(IsCheaperThan50Dollars);
+        foreach (var book in cheapBooks)
+        {
+            Console.WriteLine(book.Title);
+        }
 
-        const int factor = 5;
-        Func<int, int> fiveTimes = x => x * factor;
-        Console.WriteLine($"Using lambda expression 'fiveTimes': {fiveTimes(13)}");
+        //Func<int, int> square = number => number * number;
+        //Console.WriteLine($"Using lambda expression 'square': {square(6)}");
+
+        //const int factor = 5;
+        //Func<int, int> fiveTimes = x => x * factor;
+        //Console.WriteLine($"Using lambda expression 'fiveTimes': {fiveTimes(13)}");
     }
 }
