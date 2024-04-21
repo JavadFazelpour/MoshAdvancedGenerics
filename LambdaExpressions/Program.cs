c0636c22918c3160795b222a8e4259fca15b4edf
Davood <davoodfazelpour@gmail.com>
2024-04-21 15:19:26 +0330

    Uses Lambda expression for declaring a method.

--- LambdaExpressions/Program.cs
+++ LambdaExpressions/Program.cs
@@ -4,10 +4,17 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(Square(6));
-    }
-    static int Square(int number)
-    {
-        return number * number;
+        // args => expression
+
+        // () => expression
+        // x => expression
+        //(x,y,z) => expression
+
+        Func<int, int> square = number => number * number;
+        Console.WriteLine($"Using lambda expression 'square': {square(6)}");
+
+        const int factor = 5;
+        Func<int, int> fiveTimes = x => x * factor;
+        Console.WriteLine($"Using lambda expression 'fiveTimes': {fiveTimes(13)}");
     }
 }
