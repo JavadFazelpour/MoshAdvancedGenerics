ecab78833ecb0906d68c3d753b4f3950994c136e
Davood <davoodfazelpour@gmail.com>
2024-04-22 16:29:30 +0330

    Make a value type nullable

--- NullableTypes/Program.cs
+++ NullableTypes/Program.cs
@@ -4,6 +4,11 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        DateTime? date = null; 
+        Console.WriteLine($"Has value:{date.HasValue}");
+        Console.WriteLine($"GetValueOrDefault: {date.GetValueOrDefault()}");
+       // Console.WriteLine($"value:{date.Value}");
         
+
     }
 }
