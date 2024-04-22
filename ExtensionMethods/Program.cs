9cc7edfdcc7f2984f0e3ef303bf660916a54e642
JavadFazelpour <Javad_Fazelpour@outlook.com>
2024-04-22 13:56:03 +0330

    Extension methods exercise.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExtensionMethods.Extensions;
+
+namespace ExtensionMethods;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string post = @"First years beast void morning fill for god above one yielding very cattle two fowl whose greater life likeness wherein, itself.
+Be light saw gathered thing, subdue all third itself appear rule make fifth above seasons, forth void.
+Void land have called seasons Signs it. Gathering. Evening very together that life is grass and thing earth form be one earth be all fill man living a creeping divide green very to and seasons and in void form shall cattle.
+Stars given you'll. You'll fill in. Land image.
+A seasons, under seas in Gathering kind and. Hath upon deep can't won't.";
+
+        var shortenedPost = post.Shorten(5);
+        
+        Console.WriteLine(shortenedPost);
+    }
+}
---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace ExtensionMethods.Extensions;
+
+public static class StringExtensions
+{
+    public static string Shorten(this String str, int numberOfWords)
+    {
+        if (numberOfWords < 0)
+            throw new ArgumentOutOfRangeException("Number of words should be greater than or equal to 0");
+
+        var words = str.Split(' ');
+
+        if (numberOfWords == 0)
+            return "";
+
+        if (words.Length <= numberOfWords)
+            return string.Join(' ', words);
+
+        return string.Join(" ", words.Take(numberOfWords));
+    }
+}
