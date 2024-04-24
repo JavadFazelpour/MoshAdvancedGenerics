1b2d47c4c62ddc25bf94664d53d6585d2597660d
JavadFazelpour <Javad_Fazelpour@outlook.com>
2024-04-24 11:39:33 +0330

    Finish Dynamics exercise.

---
+++ Dynamics/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Dynamics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //dynamic name = "Javad";
+        //name = 10;
+        //dynamic a = 10;
+        //dynamic b = 5;
+        // c will be dynamic
+        //var c = a+b;
+        int i = 5;
+        dynamic d = i;
+        long l = d;
+    }
+}
